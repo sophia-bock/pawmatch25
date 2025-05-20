a103b55e7dfaaadb46a64e20e1fbc1c8c487abf2
bocks26@barker.college <bocks26@barker.college>
2025-05-20 03:38:06 +0000

    Deleted UploadPetsForm

--- client_code/UploadPetsForm/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._anvil_designer import UploadPetsFormTemplate
-from anvil import *
-import anvil.server
-
-class UploadPetsForm(UploadPetsFormTemplate):
-  def file_loader_1_change(self, file, **event_args):
-    if file is not None:
-      anvil.server.call('upload_pet_data', file)
-      alert("Pet data uploaded successfully!")
